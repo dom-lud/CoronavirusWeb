576c6d36c7396dca8f485cdfce52505912469ca1
s26964-pj <s26964@pjwstk.edu.pl>
2023-07-01 10:46:40 +0200

    Start First Project
    - Data Request
    - Fetching virus data
    - Making HTTP calls
    - Sending a request
    - Parsing csv
    - Importing data
    - String reader
    - adding schedule annotation
    - running every day
    - adding location stats
    - rendering UI in HTML

---
+++ src/test/java/com/example/coronavirusweb/CoronavirusWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronavirusweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
